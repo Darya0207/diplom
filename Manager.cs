c6a6122a1f01ec788d781b4bdbada4fe51960261
Darya0207 <147683841+Darya0207@users.noreply.github.com>
2024-06-13 22:04:03 +1000

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace STO_Koleso
+{
+    internal class Manager
+    {
+        public static Frame MainFrame;
+        public static Frame SecondFrame;
+        public static users AuthUser;
+    }
+}
